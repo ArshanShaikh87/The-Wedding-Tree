0da9399b5c65ce2b799cdef317ac5a9daa1f78df
ArshanShaikh87 <viparsh4040@gmail.com>
2025-12-29 08:47:46 +0530

    Again cloudinary changed to Url field

--- wedtree/models.py
+++ wedtree/models.py
@@ -282,7 +282,7 @@ class Gallery(models.Model):
 
     title = models.CharField(max_length=150)
     image = CloudinaryField("image", blank=True, null=True)
-    video = CloudinaryField(
+    video = models.URLField(
         blank=True,
         null=True,
         help_text="Upload video to Cloudinary and paste the URL here"
